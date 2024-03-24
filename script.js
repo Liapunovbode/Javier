fb7db9f1441ed1a9e6573817ffa07db7b8e73397
Liapunovbode <163705432+Liapunovbode@users.noreply.github.com>
2024-03-24 01:10:52 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+function pedirClave() {
+    var clave = prompt("Por favor ingresa la clave:");
+
+    // Verificar si la clave es correcta
+    if (clave === "kostenko") {
+        // Abrir enlace de descarga en otra pestaña del navegador
+        window.open("C:\\Users\\Javier\\Desktop\\CV 2024\\CuRi.pdf", "_blank");
+    } else {
+        alert("Clave incorrecta. Inténtalo de nuevo.");
+    }
+}
+
+// Vincular la función al evento click del botón
+document.getElementById("downloadButton").addEventListener("click", pedirClave);
